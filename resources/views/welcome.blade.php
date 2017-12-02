e385f4296163a0ed96ba1616934ec7cb46ad927b
Fabian JL <fabian.main@gmail.com>
2017-12-02 11:11:09 -0600

    se agrego nuevas opciones en la vista welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -73,7 +73,9 @@
                     @else
                         <a href="{{ route('login') }}">Login</a>
                         <a href="{{ route('register') }}">Register</a>
-                        <a href="{{ route('users.store') }}">create user</a>
+                        <a href="{{ route('users.store') }}">users</a>
+                        <a href="{{ route('users.create') }}">create user</a>
+
                     @endauth
                 </div>
             @endif
