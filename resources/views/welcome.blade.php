21a70706748f9a0590c80c5324419034477ad2d6
Fabian JL <fabian.main@gmail.com>
2017-12-01 23:47:55 -0600

    se agrego nueva opcion create user

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -73,6 +73,7 @@
                     @else
                         <a href="{{ route('login') }}">Login</a>
                         <a href="{{ route('register') }}">Register</a>
+                        <a href="{{ route('users.store') }}">create user</a>
                     @endauth
                 </div>
             @endif
